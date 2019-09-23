1ee3229502bab5ff44e457e4a766c6f5394de9db
Dexter Miller <dexterm@atgfw.com>
2019-09-23 13:31:12 -0400

    Starting to write and test funcitons

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ﻿# Dexer-Miller
 This branch is for the work ONLY on Tic-Tac-Toe
+
+This is a good source for vectors: https://www.oreilly.com/library/view/machine-learning-with/9781491989371/ch01.html
+
--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,18 +1,48 @@
+
 class ticTacToe: 
-    def setBoardArray()
-        #this fills the array
-    def printBoard()
+    board = [[0, 0, 0], 
+             [0, 0, 0],
+             [0, 0, 0]]
+    def setBoardArray():
+        #this fills the array and initializes it
+        #We should do a vector to make it easier to iterate through!
+        board = [[0, 0, 0], 
+             [0, 0, 0],
+             [0, 0, 0]]
+        return board
+    def printBoard(n):
         # this prints the board in ascii
-    def player1Turn()
+        # what type of graphics can i do? 
+        return n
+    def player1Turn():
         # this goes through a do while loop, until the input is correct
         # given correct input, it goes through a case switch, checking and setting place value
-    def player2Turn()
+        # Do i have to write a long switch, or can i iterate through? 
+        return board
+    def player2Turn():
         # Same as player 1 but sets value to O
-    def checkWin()
+        # same as player 1 
+        return board
+    def checkWin():
         # this function checks if there are 3 elements in a row that are equal
         # done by a lot of else if statements. 
         # Returns an int 1 or 0
-    def checkCatGame()
+        # iterative checking? 
+        win = False
+        return win
+    def isFull(n):
+        #This function is going to return true if the board is full
+        #If not the board will be empty
+        for i in range(1,3):
+            for j in range(1,3):
+                print("({},{})",i,j)
+    def checkCatGame():
         # If board is filled and no win = cats game
         # this iterates through the whole array setting the bool value
-
+        # is filled function?
+        catGame = False
+        return catGame 
+game = ticTacToe
+Board = game.board
+game.isFull(Board)
+print(game.printBoard(Board))
