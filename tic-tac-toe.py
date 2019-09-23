a2ea95f5d14bbeeeb7ec4e4c503d47120c1cc326
Dexter Miller <dexterm@atgfw.com>
2019-09-23 14:09:58 -0400

    finished checkwin and isfull functions

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -23,19 +23,41 @@ class ticTacToe:
         # Same as player 1 but sets value to O
         # same as player 1 
         return board
-    def checkWin():
+    def checkWin(n):
         # this function checks if there are 3 elements in a row that are equal
         # done by a lot of else if statements. 
         # Returns an int 1 or 0
         # iterative checking? 
         win = False
+        #Horizontal
+        for i in range(0,3):
+            j = 0
+            if n[i][j] == n[i][j+1] and n[i][j] == n[i][j+2]:
+                if n[i][j] != 0:
+                    win = True
+        #Vertical
+        for j in range(0,3):
+            i = 0
+            if n[i][j] == n[i+1][j] and n[i][j] == n[i+2][j]:
+                if n[i][j] != 0:
+                    win = True
+        #Diagonal
+        if n[0][0] == n[1][1] and n[0][0] == n[2][2]:
+                   if n[0][0] != 0:
+                        win = True
+        if n[2][0] == n[1][1] and n[2][0] == n[0][2]:
+                   if n[2][0] != 0:
+                        win = True
         return win
     def isFull(n):
         #This function is going to return true if the board is full
         #If not the board will be empty
-        for i in range(1,3):
-            for j in range(1,3):
-                print("({},{})",i,j)
+        full = True
+        for i in range(0,3):
+            for j in range(0,3):
+                if n[i][j] == 0:
+                    full = False
+        return full
     def checkCatGame():
         # If board is filled and no win = cats game
         # this iterates through the whole array setting the bool value
@@ -44,5 +66,6 @@ class ticTacToe:
         return catGame 
 game = ticTacToe
 Board = game.board
-game.isFull(Board)
+print(game.isFull(Board))
+print(game.checkWin(Board))
 print(game.printBoard(Board))
