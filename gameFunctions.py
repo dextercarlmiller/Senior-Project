216ab4ad15cce344cc9f878c1ac1e611c24b03c8
Dexter Miller <dexterm@atgfw.com>
2019-10-07 16:24:44 -0400

    fixed logistics and added labeling

--- gameFunctions.py
+++ gameFunctions.py
@@ -3,19 +3,20 @@ class ticTacToe:
     board = [[1, 2, 3], 
              [4, 5, 6],
              [7, 8, 9]]
-    Turn = 1
-    def PlayerTurn(self,n,CellID,turn):
+    def PlayerTurn(self,n,CellID,Turn,label):
         for i in range(0,3):
             for j in range(0,3):
                 if CellID == n[i][j]:
-                    if n==1:
+                    print(CellID)
+                    print(n[i][j])
+                    if Turn==1:
                         n[i][j] = "X"
-                        print(n[i][j])
-                    if n==2:
+                        label = "X"
+                    if Turn==2:
                         n[i][j] = "O"
-                    turn = ticTacToe.ChangeTurn(self,turn)    
-        return n
-
+                        label = "O"
+                    Turn = self.ChangeTurn(ticTacToe,Turn)
+        return n,Turn,label
     def ChangeTurn(self,n):
         if n == 1:
             n=2
@@ -49,12 +50,12 @@ class ticTacToe:
         full = True
         for i in range(0,3):
             for j in range(0,3):
-                if n[i][j] == 0:
-                    full = False
+                for k in range(1,10):
+                    if n[i][j] == k:
+                        full = False
         return full
     def checkCatGame(self,n):
         catGame = False
-        if self.isFull(n) and not self.checkWin(n):
+        if self.isFull(self,n) and (not self.checkWin(self,n)):
             catGame = True
         return catGame 
-
