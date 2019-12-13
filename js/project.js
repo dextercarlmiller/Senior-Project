27dfb38796e1fd0007ef97f18e29a3c60754fc37
Dexter Miller <dexterm@atgfw.com>
2019-12-13 14:10:32 -0500

    The buttons work!

--- js/project.js
+++ js/project.js
@@ -1,21 +1,21 @@
-var board;
+var board = [];
 const human = "O";
 const comp = "X";
-
 const cells = document.querySelectorAll('.cell');
 startGame();
 
 function startGame() {
+    document.querySelector(".endgame").style.display = "none";
+    // board = Array.from(Array(9).keys());
     for (var i = 1; i <= 9; i++) {
-        board[i] = i;
+        temp = board.push(i);
     }
-    //board = Array.from(Array(10).keys());
+
     for (var i = 0; i < cells.length; i++) {
-        cells[i].innerText = " ";
-        cells[i].addEventListener('click', playerturn, false);
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', playerturn, true);
     }
-    console.log(board);
-    console.log(cells);
 }
 
 function playerturn(box) {
