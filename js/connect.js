e2bbfb5876eac943e7229e2bb164df5dc7186ef0
dextercarlmiller <36194823+dextercarlmiller@users.noreply.github.com>
2020-03-03 08:51:18 -0500

    started connect 4 js

--- js/connect.js
+++ js/connect.js
@@ -1,6 +1,32 @@
-var grid = [[0,0,0,0,0,0,0],
+var gridboard = [[0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0]];
+var Winner = false; 
+const Red = "1";
+const Yellow = "2";
+var turnValue = 1;
+const connectcells = document.querySelectorAll(".connectcell")
+
+function startconnect(){
+    /*This is my constructor to reset the board and start a new game. 
+    */
+   turnValue = 1;
+   gridboard = [[0,0,0,0,0,0,0],
+   [0,0,0,0,0,0,0],
+   [0,0,0,0,0,0,0],
+   [0,0,0,0,0,0,0],
+   [0,0,0,0,0,0,0],
+   [0,0,0,0,0,0,0]];
+}
+function selectColumn(column){
+    console.log(column);
+    console.log(gridboard);
+    for (var i = 0; i<6; i++){
+        if (gridboard[i[column]] = 0 && gridboard[(i+1)[column]]){
+            gridboard[i[column]] = 1
+        }
+    }
+}
